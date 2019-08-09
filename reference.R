923d8361569150669c816999e55ec954fa7a0a62
MCTTAN <mariechantaltan@gmail.com>
2019-08-09 20:44:42 +0000

    commit

--- reference.R
+++ reference.R
@@ -14,7 +14,7 @@ m <- melt(data = nightingale, id = "Date")
 # m
 
 # Rename the columns.
-names(m) <- c("Date", "Cause of Death", "Number of Deaths")
+names(m) <- c("Date", "Cause of Death", "Death Rate")
 m
 
 m$"Cause of Death" <- sub("\\.rate", "", m$"Cause of Death")
@@ -26,19 +26,19 @@ m$Regime <- ordered( rep(c(rep('Before', 12), rep('After', 12)), 3),
 # m$Regime
 nightingale_1 <- subset(m, Date < as.Date("1855-04-01"))
 nightingale_2 <- subset(m, Date >= as.Date("1855-04-01"))
-nightingale_1 <- nightingale_1[order(nightingale_1$"Number of Deaths", decreasing = TRUE), ]
-nightingale_2 <- nightingale_2[order(nightingale_2$"Number of Deaths", decreasing = TRUE), ]
+nightingale_1 <- nightingale_1[order(nightingale_1$"Death Rate", decreasing = TRUE), ]
+nightingale_2 <- nightingale_2[order(nightingale_2$"Death Rate", decreasing = TRUE), ]
 m <- rbind(nightingale_1, nightingale_2)
 
 coxcombs_1 <- ggplot(nightingale_1,
                    aes(x = factor(Date),
-                       y = nightingale_1$"Number of Deaths",
+                       y = nightingale_1$"Death Rate",
                        fill = nightingale_1$"Cause of Death")) + geom_bar(width = 1, position = "identity", stat = "identity", color = "black") + scale_y_sqrt()
 coxcombs_1 + coord_polar(start = 3 * pi/2) + ggtitle("Causes of Mortality in the Army in the East") + xlab("")
 
 coxcombs_2 <- ggplot(nightingale_2,
                      aes(x = factor(Date),
-                         y = nightingale_2$"Number of Deaths",
+                         y = nightingale_2$"Death Rate",
                          fill = nightingale_2$"Cause of Deaths")) + geom_bar(width = 1, position = "identity", stat = "identity", color = "black") + scale_y_sqrt()
 coxcombs_2 + coord_polar(start = 3 * pi/2) + ggtitle("Causes of Mortality in the Army in the East") + xlab("")
 
